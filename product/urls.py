a3010e702ed42abe52f1b9e326a111218aa8ff4d
ilya <ilyabulatau@gmail.com>
2023-01-15 17:53:40 +0300

    fix bag - add urls path, bat im have code reapite

--- product/urls.py
+++ product/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.ProductListView.as_view(), name='home'),
     path('cat/<int:category_id>/', views.cat_detail, name='cat_detail'),
-    path('<int:pk>/', views.ProductDeatailView.as_view(), name='product')
+    path('<int:pk>/', views.ProductDeatailView.as_view(), name='product'),
+    path('cat/<int:category_id>/<int:pk>/', views.ProductDeatailView.as_view(), name='product'),
+
 ]
