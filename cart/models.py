551593938e2cf6df718b4172c0ea8a1eb5cc3741
ilya <ilyabulatau@gmail.com>
2023-01-15 22:16:05 +0300

    fix bag, now you can open the cart

--- cart/models.py
+++ cart/models.py
@@ -17,7 +17,7 @@ class CartProduct(models.Model):
     cart = models.ForeignKey(Cart, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.IntegerField()
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
 
     def sub_total(self):
             return self.product.price * self.quantity
