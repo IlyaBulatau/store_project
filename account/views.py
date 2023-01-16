04485267f0867c5ddf5a3392aa6315c7f03a3407
ilya <ilyabulatau@gmail.com>
2023-01-16 20:23:48 +0300

    mod account app

--- account/views.py
+++ account/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 
 from account import forms
 
 
-def profile(requsest):
-    form = forms.ProfileForm()
+def profile(request):
+    if request.method=='POST':
+        form = forms.ProfileForm( instance=request.user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('profile'))
+    else:
+        form = forms.ProfileForm(instance=request.user)
     context = {'form': form}
-    return render(requsest, 'account/profile.html', context)
+    return render(request, 'account/profile.html', context)
 
 
